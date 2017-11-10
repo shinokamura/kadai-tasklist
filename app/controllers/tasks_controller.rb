61dda4f817c17a8bb60ee3e2e819f022801867d8
Shin OKAMURA <threetrees1355@gmail.com>
2017-11-10 01:36:42 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def show
@@ -49,8 +49,12 @@ class TasksController < ApplicationController
   
   private
   
+   def set_task
+    @task = Task.find(params[:id])
+   end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
 
